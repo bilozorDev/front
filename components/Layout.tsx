4f9e707d8fb5568badc5cb714024ccaf97d38220
Alex Bilozor <bilazor@gmail.com>
2025-02-28 15:55:56 -0800

    username and avatar in dashboard

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -28,6 +28,8 @@ import {
   MagnifyingGlassIcon,
 } from "@heroicons/react/20/solid";
 import { usePathname } from "next/navigation";
+import { useClerk, useUser } from "@clerk/nextjs";
+
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
@@ -57,6 +59,9 @@ const userNavigation = [
 export default function Layout({ children }: { children: React.ReactNode }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const pathname = usePathname();
+  const { signOut } = useClerk();
+  const { user } = useUser();
+
   return (
     <>
       <div>
@@ -321,16 +326,16 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                   <MenuButton className="-m-1.5 flex items-center p-1.5">
                     <span className="sr-only">Open user menu</span>
                     <img
-                      alt=""
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      className="size-8 rounded-full bg-gray-50"
+                      alt={user?.fullName ?? ""}
+                      src={`https://placehold.co/600x400?text=${user?.fullName?.[0]}`}
+                      className="w-8 h-8 object-cover rounded-full overflow-hidden aspect-square bg-gray-50"
                     />
                     <span className="hidden lg:flex lg:items-center">
                       <span
                         aria-hidden="true"
                         className="ml-4 text-sm/6 font-semibold text-gray-900"
                       >
-                        Tom Cook
+                        {user?.fullName}
                       </span>
                       <ChevronDownIcon
                         aria-hidden="true"
@@ -342,16 +347,14 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                     transition
                     className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 ring-1 shadow-lg ring-gray-900/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
                   >
-                    {userNavigation.map((item) => (
-                      <MenuItem key={item.name}>
-                        <a
-                          href={item.href}
-                          className="block px-3 py-1 text-sm/6 text-gray-900 data-focus:bg-gray-50 data-focus:outline-hidden"
-                        >
-                          {item.name}
-                        </a>
-                      </MenuItem>
-                    ))}
+                    <MenuItem>
+                      <button
+                        onClick={() => signOut()}
+                        className="block px-3 py-1 text-sm/6 text-gray-900 data-focus:bg-gray-50 data-focus:outline-hidden hover:underline"
+                      >
+                        Sign out
+                      </button>
+                    </MenuItem>
                   </MenuItems>
                 </Menu>
               </div>
