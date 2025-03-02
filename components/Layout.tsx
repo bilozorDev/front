b834c672ab09494a7928d5ccab76c3325f45aba9
Alexander Bilozor <alexb@Alexanders-MacBook-Air.local>
2025-03-02 07:02:43 -0800

    added profile popup and UI for login

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -28,7 +28,7 @@ import {
   MagnifyingGlassIcon,
 } from "@heroicons/react/20/solid";
 import { usePathname } from "next/navigation";
-import { useClerk, useUser } from "@clerk/nextjs";
+import { SignedOut, SignInButton, useClerk, useUser } from "@clerk/nextjs";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -61,6 +61,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
   const { signOut } = useClerk();
   const { user } = useUser();
+  const { openUserProfile } = useClerk();
 
   return (
     <>
@@ -322,41 +323,54 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                 />
 
                 {/* Profile dropdown */}
-                <Menu as="div" className="relative">
-                  <MenuButton className="-m-1.5 flex items-center p-1.5">
-                    <span className="sr-only">Open user menu</span>
-                    <img
-                      alt={user?.fullName ?? ""}
-                      src={`https://placehold.co/600x400?text=${user?.fullName?.[0]}`}
-                      className="w-8 h-8 object-cover rounded-full overflow-hidden aspect-square bg-gray-50"
-                    />
-                    <span className="hidden lg:flex lg:items-center">
-                      <span
-                        aria-hidden="true"
-                        className="ml-4 text-sm/6 font-semibold text-gray-900"
+                {!user ?
+                  <SignInButton />
+                  : (
+                    <Menu as="div" className="relative">
+                      <MenuButton className="-m-1.5 flex items-center p-1.5" >
+                        <span className="sr-only">Open user menu</span>
+                        <img
+                          alt={user?.fullName ?? ""}
+                          src={`https://placehold.co/600x400?text=${user?.fullName?.[0]}`}
+                          className="w-8 h-8 object-cover rounded-full overflow-hidden aspect-square bg-gray-50"
+                        />
+                        <span className="hidden lg:flex lg:items-center">
+                          <span
+                            aria-hidden="true"
+                            className="ml-4 text-sm/6 font-semibold text-gray-900"
+                          >
+                            {user?.fullName}
+                          </span>
+                          <ChevronDownIcon
+                            aria-hidden="true"
+                            className="ml-2 size-5 text-gray-400"
+                          />
+                        </span>
+                      </MenuButton>
+
+                      <MenuItems
+                        transition
+                        className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 ring-1 shadow-lg ring-gray-900/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
                       >
-                        {user?.fullName}
-                      </span>
-                      <ChevronDownIcon
-                        aria-hidden="true"
-                        className="ml-2 size-5 text-gray-400"
-                      />
-                    </span>
-                  </MenuButton>
-                  <MenuItems
-                    transition
-                    className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 ring-1 shadow-lg ring-gray-900/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
-                  >
-                    <MenuItem>
-                      <button
-                        onClick={() => signOut()}
-                        className="block px-3 py-1 text-sm/6 text-gray-900 data-focus:bg-gray-50 data-focus:outline-hidden hover:underline"
-                      >
-                        Sign out
-                      </button>
-                    </MenuItem>
-                  </MenuItems>
-                </Menu>
+                        <MenuItem>
+                          <button
+                            onClick={() => openUserProfile()}
+                            className="block px-3 py-1 text-sm/6 text-gray-900 data-focus:bg-gray-50 data-focus:outline-hidden hover:underline"
+                          >
+                            User Profile
+                          </button>
+                        </MenuItem>
+                        <MenuItem>
+                          <button
+                            onClick={() => signOut()}
+                            className="block px-3 py-1 text-sm/6 text-gray-900 data-focus:bg-gray-50 data-focus:outline-hidden hover:underline"
+                          >
+                            Sign out
+                          </button>
+                        </MenuItem>
+                      </MenuItems>
+                    </Menu>
+                  )}
               </div>
             </div>
           </div>
